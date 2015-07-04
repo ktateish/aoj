c6d2e0fd1077b7f3434f5a60d32681df795d43a4
Katsuyuki TATEISHI <kt@wheel.jp>
2015-07-05 00:57:02 +0900

    Add check for existence of the test set file

--- tools/testset.sh
+++ tools/testset.sh
@@ -11,5 +11,7 @@ baseurl='http://analytic.u-aizu.ac.jp:8080/aoj/testcase_header.jsp?id='
 pid=$(get_pid $1)
 ofile="$(basename $1 .c).tset"
 
-echo "Fetching test cases info. for ${pid}"
-curl -s "${baseurl}${pid}" > "$ofile"
+if [ ! -f "$ofile" ]; then
+	echo "Fetching test cases info. for ${pid}"
+	curl -s "${baseurl}${pid}" > "$ofile"
+fi
