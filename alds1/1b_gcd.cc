1d905c0025bf8ef8c1a89b84ffdd715ee1f04ad5
Katsuyuki Tateishi <kt@wheel.jp>
2016-02-20 23:38:14 +0900

    cpp: ALDS1_1

---
+++ alds1/1b_gcd.cc
@@ -0,0 +1,29 @@
+#define dbg(...) fprintf(stderr,__VA_ARGS__)
+#define dpr(x) cerr<<#x<<": "<<x<<endl;
+#define dprc(c) do{cerr<<#c<<":";for(auto&_i:(c)){cerr<<" "<<_i;}cerr<<endl;}while(0)
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+int INF = 1e9+7;
+#define all(c) (c).begin(), (c).end()
+#define tr(i,c) for(auto i=(c).begin();i!=(c).end();i++)
+#define rtr(i,c) for(auto i=(c).rbegin();i!=(c).rend();i++)
+#define rep(i,b) for(auto i=0;i<(b);i++)
+#define pb push_back
+#define sz(c) int((c).size())
+
+int gcd(int a, int b) {
+  if (a % b == 0) return b;
+  return gcd(b, a % b);
+}
+
+int main(int argc, char **argv)
+{
+  int x, y;
+  cin >> x >> y;
+  cout << gcd(x, y) << endl;
+  return 0;
+}
+
