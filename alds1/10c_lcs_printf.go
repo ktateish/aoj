08afa887d6e6f347c046a6fc35001447740453b3
Katsuyuki Tateishi <kt@wheel.jp>
2017-09-09 17:52:15 +0900

    Add ALDS1_10_C in go

---
+++ alds1/10c_lcs_printf.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+func lcs(x, y string) string {
+	rx := []rune{}
+	for _, r := range x {
+		rx = append(rx, r)
+	}
+	ry := []rune{}
+	for _, r := range y {
+		ry = append(ry, r)
+	}
+	dp := make([][]string, len(rx)+1)
+	for i := 0; i <= len(rx); i++ {
+		dp[i] = make([]string, len(ry)+1)
+	}
+	for i := 1; i <= len(rx); i++ {
+		for j := 1; j <= len(ry); j++ {
+			if len(dp[i-1][j]) > len(dp[i][j-1]) {
+				dp[i][j] = dp[i-1][j]
+			} else {
+				dp[i][j] = dp[i][j-1]
+			}
+			if rx[i-1] == ry[j-1] && len(dp[i-1][j-1])+1 > len(dp[i][j]) {
+				dp[i][j] = fmt.Sprintf("%s%c", dp[i-1][j-1], rx[i-1])
+			}
+		}
+	}
+	return dp[len(rx)][len(ry)]
+}
+
+func main() {
+	defer stdout.Flush()
+	q := readInt()
+	for i := 0; i < q; i++ {
+		x := readString()
+		y := readString()
+		println(len(lcs(x, y)))
+	}
+}
+
+var (
+	readString func() string
+	stdout     *bufio.Writer
+)
+
+func init() {
+	readString = newReadString(os.Stdin)
+	stdout = bufio.NewWriter(os.Stdout)
+}
+
+func newReadString(ior io.Reader) func() string {
+	r := bufio.NewScanner(ior)
+	r.Split(bufio.ScanWords)
+
+	return func() string {
+		if !r.Scan() {
+			panic("Scan failed")
+		}
+		return r.Text()
+	}
+}
+
+func readInt() int {
+	i, err := strconv.Atoi(readString())
+	if err != nil {
+		panic(err.Error())
+	}
+	return i
+}
+
+func readIntSlice(n int) []int {
+	b := make([]int, n)
+	for i := 0; i < n; i++ {
+		b[i] = readInt()
+	}
+	return b
+}
+
+func readLengthAndSlice() []int {
+	n := readInt()
+	return readIntSlice(n)
+}
+
+func printf(f string, args ...interface{}) (int, error) {
+	return fmt.Fprintf(stdout, f, args...)
+}
+
+func println(args ...interface{}) (int, error) {
+	return fmt.Fprintln(stdout, args...)
+}
